4efb1de5953439c96cdb090cbace4e2544ba6fab
Trent Weiss <ttw2xk@virginia.edu>
2019-06-10 10:59:14 -0400

    node compiles

--- src/pure_pursuit_controller_node.cpp
+++ src/pure_pursuit_controller_node.cpp
@@ -22,7 +22,7 @@
 
 #include <ros/ros.h>
 
-#include "PurePursuitControllerNode.h"
+#include "pure_pursuit_controller/PurePursuitControllerNode.h"
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "pure_pursuit_controller");
